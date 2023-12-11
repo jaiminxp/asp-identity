f0b081b0d357f14e88d4d66bccd09d5b0674b1e5
Jaimin Parmar <jaimins100@gmail.com>
2023-12-11 23:26:36 +0530

    Configure the confirmation email sender

--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesPizza.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using RazorPagesPizza.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection");
@@ -13,6 +15,7 @@ builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.Sign
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
