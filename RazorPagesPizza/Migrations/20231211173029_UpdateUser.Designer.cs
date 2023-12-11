2e762ec2303b2cce5924c55c15faea0d7cef3f4f
Jaimin Parmar <jaimins100@gmail.com>
2023-12-11 23:00:57 +0530

    add and apply UpdateUser migration

--- RazorPagesPizza/Migrations/20231211173029_UpdateUser.Designer.cs
+++ RazorPagesPizza/Migrations/20231211173029_UpdateUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesPizza.Areas.Identity.Data;
 
@@ -10,9 +11,10 @@ using RazorPagesPizza.Areas.Identity.Data;
 namespace RazorPagesPizza.Migrations
 {
     [DbContext(typeof(RazorPagesPizzaAuth))]
-    partial class RazorPagesPizzaAuthModelSnapshot : ModelSnapshot
+    [Migration("20231211173029_UpdateUser")]
+    partial class UpdateUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.3");
